92f3f919497bf8559260d70d911958f32a4be859
USER <user@dh101148.sk.tsukuba.ac.jp>
2015-02-19 19:39:18 +0900

    Initial commit

---
+++ app/src/main/java/com/tsukuba/gourmet/MapsActivity.java
@@ -0,0 +1,46 @@
+package com.tsukuba.gourmet;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        LatLng tsukuba = new LatLng(36.089275,140.074928);               //つくば市付近の緯度経度。15倍ズーム。
+        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(tsukuba,15);
+        mMap.moveCamera(cu);
+        // mMap.addMarker(new MarkerOptions().position(tsukuba).title("つくば"));       //マーカー
+    }
+}
